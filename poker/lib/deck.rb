3b12edfbcc74e700790244caa242891e806012a1
Dan Holodak <danholodak@gmail.com>
2023-02-02 17:22:55 -0500

    Sort files, Progress on poker

--- poker/lib/deck.rb
+++ poker/lib/deck.rb
@@ -1,12 +1,20 @@
 #class deck
+require 'card'
 
 class Deck
     
     values = (1..13).to_a
     SUITS = [:heart, :club, :diamond, :spade]
     VALUES = values
+    attr_reader :deck
     def initialize
-
+        @deck = []
+        suits.each do |suit|
+            values.each do |value|
+                @deck << Card.new(suit, value)
+            end
+        end
+        7.times {@deck.shuffle!}
     end
 
     def suits
--- poker/spec/deck_spec.rb
+++ poker/spec/deck_spec.rb
@@ -4,8 +4,8 @@ describe "Deck" do
     subject(:deck) {Deck.new}
     describe "#intialize" do
         it "creates an array and fills it with 52 unique cards" do
-            expect(deck.cards.uniq.length).to be(52)
-            expect(deck.cards.length).to be(52)
+            expect(deck.deck.uniq.length).to be(52)
+            expect(deck.deck.length).to be(52)
         end
     end
 end
